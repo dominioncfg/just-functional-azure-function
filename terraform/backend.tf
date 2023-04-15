d0ce98efe583e4c5c57329318e5cd35c2b7fd33f
Jose Carlos <josecdom94@gmail.com>
2023-04-15 12:09:31 +0200

    Backend Config from Vars

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "just-functional-infra-rg"
-    storage_account_name = "stjustfunctionaltf"
-    container_name       = "tfstate"
-    key                  = "functions.tfstate"
+    #resource_group_name  = "just-functional-infra-rg"
+    #storage_account_name = "stjustfunctionaltf"
+    #container_name       = "tfstate"
+    #key                  = "functions.tfstate"
   }
 
 }
