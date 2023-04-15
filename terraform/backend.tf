654ddc3524e8b383915e4634ad21def0898b4edd
Jose Carlos <josecdom94@gmail.com>
2023-04-15 07:51:14 +0200

    Push And Plan

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,9 @@
-backend "azurerm" {
-  resource_group_name  = "just-functional-infra-rg"
-  storage_account_name = "stjustfunctionaltf"
-  container_name       = "tfstate"
-  key                  = "functions.tfstate"
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "just-functional-infra-rg"
+    storage_account_name = "stjustfunctionaltf"
+    container_name       = "tfstate"
+    key                  = "functions.tfstate"
+  }
+
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,72 @@
-provider "azurerm" {  
-  version = "~>2.0"
+##################################################################################
+# Terrraform Config
+##################################################################################
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "azurerm" {
   features {}
 }
- 
+
+
 data "azurerm_client_config" "current" {}
+
+
+##################################################################################
+# Variables
+##################################################################################
 
 variable "default_location" {
   type    = string
   default = "francecentral"
 }
- 
-#Create Resource Group
-resource "azurerm_resource_group" "tamops" {
-  name     = "rg-${var.application_name}"
-  location = var.francecentral
+
+variable "application_name" {
+  type    = string
+  default = "justfunction"
+}
+
+variable "environment_name" {
+  type    = string
+  default = "Production"
 }
 
 
+##################################################################################
+# Locals
+##################################################################################
+
+locals {
+  environments = {
+    Development = "dev"
+    Production  = "prod"
+  }
+
+  rnd_number = "001"
+}
+
+
+locals {
+  environment_prefix = lookup(local.environments, var.environment_name, local.environments["Development"])
+}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+#Create Resource Group
+resource "azurerm_resource_group" "rgApp" {
+  name     = "rg-${var.application_name}-${local.environment_prefix}-${local.rnd_number}"
+  location = var.default_location
+}
